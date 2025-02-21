703bfe337a877c47651bf2d8f1d7c6b798dac641
Matheus Otenio <matheus.otenio843@gmail.com>
2025-02-21 15:05:08 -0300

    personalização teste

--- src/app/[slug]/menu/components/header.tsx
+++ src/app/[slug]/menu/components/header.tsx
@@ -17,7 +17,7 @@ const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
   const handleBackClick = () => router.back();
   const handleOrdersClick = () => router.push(`/${slug}/orders`);
   return (
-    <div className="relative h-[250px] w-full">
+    <div className="relative h-[250px] w-full bg-cover bg-bottom sm:h-[400px] lg:h-[650px]">
       <Button
         variant="secondary"
         size="icon"
